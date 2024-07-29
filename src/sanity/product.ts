28eac576608d651a5d97af8e7861f7be3e59b07e
tehrimzahra <tehrimzahra984@gmail.com>
2024-07-29 09:08:19 +0500

    new project

---
+++ src/sanity/product.ts
@@ -0,0 +1,22 @@
+export const product ={
+    name:"product",
+    type:"document",
+    title:"Product",
+    fields:[
+        {
+            name:"title",
+            title:"Title",
+            type:"string",
+        },
+        {
+            name:"description",
+            title:"Product Description",
+            type:"string"
+        },
+        {
+            name:"image",
+            title:"product Image",
+            type:"image"
+        }
+    ]
+}
